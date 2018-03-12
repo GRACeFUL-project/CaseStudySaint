a035be5390045dd37ced68a4753f441bf2ae7bd7
Alex Gerdes <agerdes@me.com>
2018-03-12 09:48:40 +0100

    Added a lib for the fish.

--- app/ServantAPI.hs
+++ app/ServantAPI.hs
@@ -3,13 +3,19 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module ServantAPI where
+
+import Prelude hiding (flip)
 
 import Servant
 import Servant.HTML.Lucid
 import Control.Monad.IO.Class
 
+import Fish
+import Image
+import Geometry
 import Saint
 
 {- Saint stuff -}
@@ -20,6 +26,38 @@ lib = Library "lists"
   [ Item "map"   $ map ::: (int --> int) --> list int --> list int
   , Item "range" $ (\a b -> [a..b]) ::: int --> int --> list int
   , Item "plus"  $ (+) ::: int --> int --> int
+  ]
+
+type Universe = A0 Float :+: A0 Image :+: A0 Point
+
+image :: A0 Image :< t => Type t Image
+image = Base (inject A0)
+
+point :: A0 Point :< t => Type t Point
+point = Base (inject A0)
+
+float :: A0 Float :< t => Type t Float
+float = Base (inject A0)
+
+fishLib :: Library Universe
+fishLib = Library "fish"
+  [ -- Create images
+    Item "blank"          $ blank          ::: image
+  , Item "addCubicBezier" $ addCubicBezier ::: point --> point --> point --> point --> image --> image
+  , Item "overlay"        $ overlay        ::: image --> image --> image
+
+    -- Hendersons functional geometry
+  , Item "flip"    $ flip    ::: image --> image
+  , Item "over"    $ over    ::: image --> image --> image
+  , Item "besideS" $ besideS ::: float --> float --> image --> image --> image
+  , Item "beside"  $ beside  ::: image --> image --> image
+  , Item "aboveS"  $ aboveS  ::: float --> float --> image --> image --> image
+  , Item "above"   $ above   ::: image --> image --> image
+  , Item "rot"     $ rot     ::: image --> image
+  , Item "rot45"   $ rot45   ::: image --> image
+
+    -- The fish image
+  , Item "fish"    $ fish ::: image
   ]
 
 runFun :: String -> String
