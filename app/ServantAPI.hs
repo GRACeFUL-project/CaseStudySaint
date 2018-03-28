3c2e43749772a8be5f8b3e095e56a245057e6d85
Alex Gerdes <agerdes@me.com>
2018-03-28 16:00:12 +0200

    Adapted example to fixed natrec.

--- app/ServantAPI.hs
+++ app/ServantAPI.hs
@@ -52,14 +52,10 @@ fishLib = Library "fish"
   , Item "rot"     $ rot     ::: image --> image
   , Item "rot45"   $ rot45   ::: image --> image
   , Item "natrec"  $ natrec  ::: image --> (int --> image --> image) --> int --> image
-  , Item "dec"     $ dec     ::: int --> int
 
     -- The fish base image
   , Item "fish"    $ fish ::: image
   ]
-
-dec :: Int -> Int
-dec n = n - 1
 
 runFish :: String -> IO Value
 runFish s = case run image fishLib s of
@@ -91,11 +87,10 @@ squarelimit =
   \let fish3 = rot (rot (rot fish2)) in\n\
   \let t     = over fish (over fish2 fish3) in\n\
   \let u     = over (over fish2 (rot fish2)) (over (rot (rot fish2)) (rot (rot (rot fish2)))) in\n\
-  \let qrt   = \\ p . \\ q . \\ r . \\ s . above (beside p q) (beside r s) in\n\
-  \let cyc   = \\ p . qrt p (rot p) (rot (rot p)) (rot (rot (rot p))) in\n\
-  \let side  = \\ n . natrec blank (\\ n . \\ img . qrt img img (rot t) t) n in\n\ 
-  \let corn  = \\ n . natrec blank (\\ n . \\ img . qrt img (side (dec n)) (rot (side (dec n))) u) n in\n\ 
-  \let nnet  = \\ p . \\ q . \\ r . \\ s . \\ t . \\ u . \\ v . \\ w . \\ x . aboveS 1 2 (besideS 1 2 p (beside q r)) (above (besideS 1 2 s (beside t u)) (besideS 1 2 v (beside w x))) in\n\
-  \let sqrl  = \\ n . nnet (corn n) (side n) (rot (rot (rot (corn n)))) (rot (side n)) u (rot (rot (rot (side n)))) (rot (corn n)) (rot (rot (side n))) (rot (rot (corn n))) in\n\
+  \let qrt   = \\\\ p . \\\\ q . \\\\ r . \\\\ s . above (beside p q) (beside r s) in\n\
+  \let cyc   = \\\\ p . qrt p (rot p) (rot (rot p)) (rot (rot (rot p))) in\n\
+  \let side  = \\\\ n . natrec blank (\\\\ n . \\\\ img . qrt img img (rot t) t) n in\n\ 
+  \let corn  = \\\\ n . natrec blank (\\\\ n . \\\\ img . qrt img (side n) (rot (side n)) u) n in\n\ 
+  \let nnet  = \\\\ p . \\\\ q . \\\\ r . \\\\ s . \\\\ t . \\\\ u . \\\\ v . \\\\ w . \\\\ x . aboveS 1 2 (besideS 1 2 p (beside q r)) (above (besideS 1 2 s (beside t u)) (besideS 1 2 v (beside w x))) in\n\
+  \let sqrl  = \\\\ n . nnet (corn n) (side n) (rot (rot (rot (corn n)))) (rot (side n)) u (rot (rot (rot (side n)))) (rot (corn n)) (rot (rot (side n))) (rot (rot (corn n))) in\n\
   \scale (sqrl 3)"
-
